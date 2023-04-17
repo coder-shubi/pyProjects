9938b1e96a3a359729ab3ec8895a9e190e9c5800
Subham Kedia <poisionman531@gmail.com>
2023-04-17 23:43:19 +0545

    new commit

--- 1.py
+++ 1.py
@@ -3,6 +3,8 @@ print("Welcome to this script that does stuff that is absolutly not going to com
 while True:
     print("Specify the commands you would like to choose. Type help for information on all commands ")
     command = input("> ")
+
+
     if command == "help":
         print('''
 number      command
@@ -17,6 +19,8 @@ number      command
 9           
 10          
         ''')
+
+
     elif command =="word":
         print("Type your word here!")
         word = input("> ")
@@ -31,6 +35,27 @@ number      command
                 print(word.capitalize())
             else:
                 print("Can't understand m8")
+
+
+    elif command =="print_pattern":
+        print("Which pattern would you like")
+        pattern_commad = input("> ")
+        if pattern_commad == "square":
+            n = 5
+            for i in range(n):
+                for j in range(n):
+                    print("*", end="  ")
+                print()
+        if pattern_commad == "left triangle":
+            n = 5
+            for i in range(n):
+                for j in range(i+1):
+                    print("*", end="  ")
+                print()
+        else:
+            print("sorry not added yet!")
+
+
     elif command == "quit":
         break
     else:
